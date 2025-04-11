e6a3e464d922c5e7d936672312df1b3632ea6882
Sunwoo Yeom <o55306605@gmail.com>
2025-04-11 11:13:29 -0400

    firebase setup + migrating inclass13 work for sign in

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     id("com.android.application")
+    // START: FlutterFire Configuration
+    id("com.google.gms.google-services")
+    // END: FlutterFire Configuration
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
@@ -9,6 +12,7 @@ android {
     namespace = "com.example.hw4"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -28,6 +32,8 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        minSdkVersion(23)
+
     }
 
     buildTypes {
